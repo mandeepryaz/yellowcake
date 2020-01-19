61a0da9e15b206fcc7a23fad049028fd645ee7d8
Mandeep Singh <mandeep@ryaz.io>
2020-01-19 21:27:43 +0530

    config changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,8 +2,10 @@ const postcssPresetEnv = require('postcss-preset-env')
 
 module.exports = {
   siteMetadata: {
-    title: 'Yelloecake',
-    siteUrl: 'https://yellowcake.netlify.com'
+      title: 'Yelloecake',
+      description: 'Yellowcake website description',
+      footer: 'yellowcake footer',
+      siteUrl: 'https://omni-blog-dev.netlify.com'
   },
   plugins: [
     'gatsby-plugin-react-helmet',
